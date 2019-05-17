2fadc4dda01c5223f1ac8350e5493dcc4145cd8d
Michael Elsam <michaelelsam@btinternet.com>
2019-05-17 17:58:58 +0100

    add a slideshow on the home page and location map on location ppage

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ pageHeading.addEventListener('click', function() {
     }
 });
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    // let i;
+    let slides = document.querySelectorAll('.mySlides');
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display= "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 7000);
+}
